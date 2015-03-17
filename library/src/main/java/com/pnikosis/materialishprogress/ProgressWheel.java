16d18a20e8287ed4b998d0b8fddbb599ab9b234e
rfernandez <rfernandez@tuenti.com>
2015-03-17 15:30:49 +0100

    remove appcompat dependency and unused imports

--- library/src/main/java/com/pnikosis/materialishprogress/ProgressWheel.java
+++ library/src/main/java/com/pnikosis/materialishprogress/ProgressWheel.java
@@ -11,7 +11,6 @@ import android.os.Parcelable;
 import android.os.SystemClock;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 
